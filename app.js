79ac5d3b1257bd0dd79e57e4577f4db5c8e5935d
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-05 23:43:44 +0500

    style added

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-var text = document.getElementById("textArea")
-console.log(text.value)
+var text = document.getElementById("textArea");
+var arr = [];
 
 function upload(){
-    text.innerHTML =  `
-    ${text.value}
-    `
+// arr.push(text.value.split("\n"));
+var value = text.value.split("\n");
+// var obj = {};
+// for(var i=0; i< value.length; i++){
+//     if(value[i].indexOf("?") !== -1){
+//         obj.question = value[i]
+//     }else{
+//         if(value[i].indexOf("*") !== -1){
+//             obj.answer = value[i].slice(0,-1)
+//             obj["option" + i] = value[i].slice(0,-1)
+//         }
+//         else{
+//             obj["option" + i] = value[i]
+//         }
+//     }
+// }
+console.log(obj)
 }
