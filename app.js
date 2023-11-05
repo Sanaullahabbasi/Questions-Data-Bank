8dc084d6ec21b9386982e2d37258d9a1e7dc1042
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-05 23:44:48 +0500

    updated

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ var arr = [];
 function upload(){
 // arr.push(text.value.split("\n"));
 var value = text.value.split("\n");
-// var obj = {};
-// for(var i=0; i< value.length; i++){
-//     if(value[i].indexOf("?") !== -1){
-//         obj.question = value[i]
-//     }else{
-//         if(value[i].indexOf("*") !== -1){
-//             obj.answer = value[i].slice(0,-1)
-//             obj["option" + i] = value[i].slice(0,-1)
-//         }
-//         else{
-//             obj["option" + i] = value[i]
-//         }
-//     }
-// }
+var obj = {};
+for(var i=0; i< value.length; i++){
+    if(value[i].indexOf("?") !== -1){
+        obj.question = value[i]
+    }else{
+        if(value[i].indexOf("*") !== -1){
+            obj.answer = value[i].slice(0,-1)
+            obj["option" + i] = value[i].slice(0,-1)
+        }
+        else{
+            obj["option" + i] = value[i]
+        }
+    }
+}
 console.log(obj)
 }
