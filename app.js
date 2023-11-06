1416b0a7cf432d4cabeaf84ee4b5dd17a5f04c40
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-06 19:11:17 +0500

    QDB updated

--- app.js
+++ app.js
@@ -5,29 +5,31 @@ function upload(){
 // arr.push(text.value.split("\n"));
 var value = text.value.split("\n");
 var obj = {};
+var count = 1;
 for(var i=0; i< value.length; i++){
     if(value[i].indexOf("?") !== -1){
         obj.question = value[i]
     }else{
         if(value[i].indexOf("*") !== -1){
             obj.answer = value[i].slice(0,-1)
-            obj["option" + i] = value[i].slice(0,-1)
+            obj["option" + count] = value[i].slice(0,-1)
         }
         else{
-            obj["option" + i] = value[i]
+            obj["option" + count] = value[i]
         }
     }
+    count++;
 }
-console.log(obj)
+console.log(obj);
 }
 
-var msg = "Read Intruction before proceed to read instruction"
-Swal.fire({
-    title: msg,
-    showClass: {
-      popup: 'animate__animated animate__fadeInDown'
-    },
-    hideClass: {
-      popup: 'animate__animated animate__fadeOutUp'
-    }
-  })
+// var msg = "Read Intruction before proceed to read instruction"
+// Swal.fire({
+//     title: msg,
+//     showClass: {
+//       popup: 'animate__animated animate__fadeInDown'
+//     },
+//     hideClass: {
+//       popup: 'animate__animated animate__fadeOutUp'
+//     }
+//   })
