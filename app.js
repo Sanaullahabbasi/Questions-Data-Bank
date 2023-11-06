8234c096936d4fb5cf68c8bd32795287f9f879d1
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-06 19:28:24 +0500

    question programming completed

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var text = document.getElementById("textArea");
-var arr = [];
 
 function upload(){
-// arr.push(text.value.split("\n"));
+    // arr.push(text.value.split("\n"));
+var questions = [];
 var value = text.value.split("\n");
 var obj = {};
 var count = 1;
 for(var i=0; i< value.length; i++){
-    if(value[i].indexOf("?") !== -1){
-        obj.question = value[i]
+    if(value[i] === ""){
+        questions.push(obj);
+        obj= {};
+        count =1
     }else{
+        if(value[i].indexOf("?") !== -1){
+            obj.question = value[i]
+        }else{
         if(value[i].indexOf("*") !== -1){
             obj.answer = value[i].slice(0,-1)
             obj["option" + count] = value[i].slice(0,-1)
@@ -17,19 +22,20 @@ for(var i=0; i< value.length; i++){
         else{
             obj["option" + count] = value[i]
         }
+        count++;
     }
-    count++;
 }
-console.log(obj);
+}
+console.log(questions);
 }
 
-// var msg = "Read Intruction before proceed to read instruction"
-// Swal.fire({
-//     title: msg,
-//     showClass: {
-//       popup: 'animate__animated animate__fadeInDown'
-//     },
-//     hideClass: {
-//       popup: 'animate__animated animate__fadeOutUp'
-//     }
-//   })
+var msg = "Rules: place question mark at the end or every question, enter a space after complete typing the questions and also place enter after the last question in your data bank"
+Swal.fire({
+    title: msg,
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
